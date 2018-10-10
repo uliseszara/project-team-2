2e241b6b0e672501cf97d6984e85fe4e3f0018ab
Caleb <cshultz117@gmail.com>
2018-10-10 12:22:54 -0700

    basic ship class stuff

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -5,11 +5,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class Ship {
+	private
+		String type, orientation;
+		int length;
+		int xCord;
+		int yCord;
 
 	@JsonProperty private List<Square> occupiedSquares;
+
+	public int getLength(){
+		return length;
+	}
+	public int getxCord(){
+		return xCord;
+	}
+	public int getyCord(){
+		return yCord;
+	}
+
+
 	
 	public Ship(String kind) {
-		//TODO implement
+		type = kind;
+		if(kind == )
 	}
 
 	public List<Square> getOccupiedSquares() {
