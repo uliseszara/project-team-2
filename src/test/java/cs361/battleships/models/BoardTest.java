6ea0d3ceb90a9ea80943d9266e73f009c27f0b8d
uliseszara <Ulises@10-248-164-198.wireless.oregonstate.edu>
2018-10-11 12:41:05 -0700

    boardTest.java file has implemented tests for ships being placed over ships, ships going out of bounds, and number/type of ships on board

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -3,12 +3,35 @@ package cs361.battleships.models;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 
 public class BoardTest {
 
     @Test
     public void testInvalidPlacement() {
         Board board = new Board();
-        assertFalse(board.placeShip(new Ship("MINESWEEPER"), 11, 'C', true));
+        assertFalse(board.placeShip(new Ship("MINESWEEPER"), 10, 'J', false));
+    }
+
+    @Test
+    public void testValidPlacement() {
+        Board board = new Board();
+        assertTrue(board.placeShip(new Ship("BATTLESHIP"), 7, 'G', false));
+    }
+
+    @Test
+    public void testInvalidPlacement2() {
+        Board board = new Board();
+        board.placeShip(new Ship("m"),1,'A',true);
+        board.placeShip(new Ship("d"),1,'B',true);
+        assertFalse(board.placeShip(new Ship("BATTLESHIP"), 3, 'A', false));
+    }
+    @Test
+    public void testInvalidPlacement3() {
+        Board board = new Board();
+        board.placeShip(new Ship("m"),1,'A',true);
+        board.placeShip(new Ship("d"),1,'B',true);
+        assertFalse(board.placeShip(new Ship("d"), 9, 'A', false));
     }
 }
