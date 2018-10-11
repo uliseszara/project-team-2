cb50e9fe9446ad0367e4e78c9951ddbb121a752c
Caleb <cshultz117@gmail.com>
2018-10-10 17:28:12 -0700

    Refining Ship class and tests

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -4,34 +4,40 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
-public class Ship {
-	private
-		String type, orientation;
-		int length;
-		int xCord;
-		int yCord;
+import static java.lang.System.exit;
 
+public class Ship {
+	private String kind;
+	private	int length;
+	private	int numHits;
 	@JsonProperty private List<Square> occupiedSquares;
 
-	public int getLength(){
-		return length;
-	}
-	public int getxCord(){
-		return xCord;
-	}
-	public int getyCord(){
-		return yCord;
-	}
+	public String getKind(){ return this.kind; }
+	public int getLength(){ return this.length; }
+	public int getNumHits(){return this.numHits;}
+    public List<Square> getOccupiedSquares() { return this.occupiedSquares; }
 
+    public void setKind(String k){ this.kind = k; }
+    public void setNumHits(int n){ this.numHits = n;}
+	public void setOccupiedSquares(List<Square> list1){ this.occupiedSquares = list1; }
+
+	public void incNumHits(){ this.numHits++; }
 
 	
-	public Ship(String kind) {
-		type = kind;
-		if(kind == )
-	}
+	public Ship(String k) {
+		k = k.toLowerCase();
 
-	public List<Square> getOccupiedSquares() {
-		//TODO implement
-		return null;
+		this.kind = k;
+		this.numHits = 0;
+		this.occupiedSquares = null;
+
+		if((k.equals("minesweeper")) || (k.equals("m"))){ this.length = 2; }
+		else if((k.equals("destroyer")) || (k.equals("d"))){ this.length = 3; }
+        else if((k.equals("battleship"))|| (k.equals("b"))){ this.length = 4; }
+        else{
+            System.out.print("Invalid ship argument");
+            exit(0);
+        }
+
 	}
 }
